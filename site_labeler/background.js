4e0ecd6a27fb9d2912a37bf944aefa56929f9455
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-05-27 21:30:51 -0400

    Continuation commit of chrome plugin tutorial

--- site_labeler/background.js
+++ site_labeler/background.js
@@ -6,7 +6,17 @@
 
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The color is green.");
+		console.log("the color is green.");
+	});
+
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {hostEquals: 'developer.chrome.com'},
+			})
+			],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
 	});
 });
 
