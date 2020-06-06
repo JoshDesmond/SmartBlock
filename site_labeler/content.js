58a8f9ae219df61c5d82635c5dba119f433bc13c
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-06-05 22:58:00 -0400

    Refactors, cleanup, & addition of flags display

--- site_labeler/content.js
+++ site_labeler/content.js
@@ -1,6 +1,9 @@
 /** The program logic for static text analysis & labeling */
 class Model {
 
+    flags = false;
+    voteAlreadySubmitted = false;
+
     /**
      * Determines what voting actions are available based on the webpage, its content, and the
      * users settings.
@@ -36,6 +39,11 @@ class Model {
         const text = this.extractText();
         const words = text.split(" ");
         return words.length;
+    }
+
+    /** Toggles the status of whether flags are enabled or not **/
+    toggleFlags() {
+        this.flags = !this.flags;
     }
 }
 
@@ -95,12 +103,13 @@ class Views {
     }
 
 
-    /**
-     * Determines the appropriate voting actions that can be made
-     * @returns {undefined}
-     */
-    getVotingActions() {
-        return undefined;
+    /** Updates the display of the footer to reflect the state of the model */
+    repaintFlags() {
+        if (this._model.flags === true) {
+            this.footerDiv.style.backgroundColor = 'red';
+        } else {
+            this.footerDiv.style.backgroundColor = 'black';
+        }
     }
 
 
@@ -109,8 +118,7 @@ class Views {
      * analysis view
      */
     addTextualAnalysisButton() {
-        const ac = new AnalysisController(this._model);
-        this.footerDiv.onclick = (() => ac.onClick());
+
     }
 
 }
@@ -121,27 +129,60 @@ class Views {
  */
 class AnalysisController {
 
-    _model;
-
     /**
      * Instantializes the controller for a given model/view
      *
      * @param {!Model} model The model of the web page for analysis
      */
     constructor(model) {
-        console.log(model);
         this._model = model;
-        console.log(this._model);
     }
 
     onClick() {
-        console.log(this);
-        console.log(this._model);
         console.log(this._model.extractTitle());
         console.log(this._model.extractText());
+    }
+}
+
+class FlagsController {
+    /**
+     *
+     * @param {!Model} model The model of associated webpage being labeled
+     * @param {!Views} views The view object to update the display of
+     */
+    constructor(model, views) {
+        this._model = model;
+        this._views = views;
+    }
+
+    toggle() {
+        this._model.toggleFlags();
+        this._views.repaintFlags();
     }
 }
 
 
 const model = new Model();
 const views = new Views(model);
+
+// Add controllers
+const ac = new AnalysisController(model);
+views.footerDiv.onclick = (() => ac.onClick());
+
+const fc = new FlagsController(model, views);
+
+// Add hotkeys
+document.onkeyup = function (e) {
+    // TODO switch to case statement?
+    if (e.key === "d") {
+        model.handleVote(window.location.href, model.question, "Not Relevant", false);
+    } else if (e.key === "j") {
+        model.handleVote(window.location.href, model.question, "Slightly Relevant", false);
+    } else if (e.key === "k") {
+        model.handleVote(window.location.href, model.question, "Very Relevant", false);
+    } else if (e.key === "Backspace" || e.key === "Delete") {
+        model.undoLastVote();
+    } else if (e.key === " ") {
+        fc.toggle();
+    }
+};
