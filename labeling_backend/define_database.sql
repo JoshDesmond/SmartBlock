e5be90373c3ca90ebba6491f6449901027345745
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-08-11 22:20:06 -0400

    Added some constraints to DDL

--- labeling_backend/define_database.sql
+++ labeling_backend/define_database.sql
@@ -1,36 +1,44 @@
-CREATE TABLE Webpages (
+CREATE TABLE IF NOT EXISTS Webpages (
     Url VARCHAR PRIMARY KEY,
-    Domain VARCHAR,
-    FlagsId INTEGER,
-    FOREIGN KEY (FlagsId) REFERENCES Flags(FlagsID)
+    Domain VARCHAR NOT NULL,
+    FlagsId INTEGER NOT NULL,
+    FOREIGN KEY (FlagsId) REFERENCES Flags(FlagsId)
 );
 
-CREATE TABLE Snapshots (
+CREATE TABLE IF NOT EXISTS Snapshots (
     SnapshotId INTEGER PRIMARY KEY,
-    Url VARCHAR,
-    DateTime INTEGER,
-    Title VARCHAR,
+    Url VARCHAR NOT NULL,
+    DateTime INTEGER NOT NULL,
+    Title VARCHAR NOT NULL,
     ContentRaw TEXT,
     FOREIGN KEY (Url) REFERENCES Webpages(Url)
 );
 
-CREATE TABLE Labels(
+CREATE TABLE IF NOT EXISTS Labels(
     LabelId INTEGER PRIMARY KEY,
-    PrimaryVote INTEGER,
-    SecondaryVote INTEGER,
-    IsObvious INTEGER, --Boolean
-    IsAmbiguous INTEGER, --Boolean
+    PrimaryVote INTEGER NOT NULL,
+    SecondaryVote INTEGER, -- Optional
+    IsObvious INTEGER DEFAULT 0, --Boolean
+    IsAmbiguous INTEGER DEFAULT 0, --Boolean
     Topic VARCHAR, --Optional
-    SnapshotId INTEGER,
-    FOREIGN KEY (SnapshotId) REFERENCES Snapshots(SnapshotId)
+    SnapshotId INTEGER NOT NULL,
+    FOREIGN KEY (SnapshotId) REFERENCES Snapshots(SnapshotId),
+    CHECK (PrimaryVote <= 4 AND
+           SecondaryVote <= 4 AND
+           IsObvious <= 1 AND
+           IsAmbiguous <= 1)
 );
 
-CREATE TABLE Flags(
-    FlagsID INTEGER PRIMARY KEY,
-    IsVeryAmbiguous INTEGER, --Boolean
-    IsReviewable INTEGER, --Boolean
-    IsNotTextual INTEGER, --Boolean
-    IsInteresting INTEGER, --Boolean
-    Url VARCHAR,
-    FOREIGN KEY (Url) REFERENCES Webpages(Url)
+CREATE TABLE IF NOT EXISTS Flags(
+    FlagsId INTEGER PRIMARY KEY,
+    IsVeryAmbiguous INTEGER DEFAULT 0, --Boolean
+    IsReviewable INTEGER DEFAULT 0, --Boolean
+    IsNotTextual INTEGER DEFAULT 0, --Boolean
+    IsInteresting INTEGER DEFAULT 0, --Boolean
+    Url VARCHAR NOT NULL,
+    FOREIGN KEY (Url) REFERENCES Webpages(Url),
+    CHECK (IsVeryAmbiguous <= 1 AND
+           IsReviewable <= 1 AND
+           IsNotTextual <= 1 AND
+           IsInteresting <= 1)
 );
