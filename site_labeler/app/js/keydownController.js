935d5e7a5daa3f50a93a478e9224df0ec01cc06b
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-08-13 12:51:15 -0400

    Fleshed out keydown controller

--- site_labeler/app/js/keydownController.js
+++ site_labeler/app/js/keydownController.js
@@ -1,3 +1,6 @@
+/**
+ * Keylistener for the standard labeling workflow.
+ */
 class KeydownController {
 
     /**
@@ -10,36 +13,54 @@ class KeydownController {
 
     /** Event listener for the windows 'keydown' event. */
     handleEvent(e) {
-        if (e.target === document.body) {
-            switch (e.key) {
-                case 'd':
-                    e.preventDefault();
-                    this.model.handleVote(1);
-                    break;
-                case 'f':
-                    e.preventDefault();
-                    this.model.handleVote(2);
-                    break;
-                case 'j':
-                    e.preventDefault();
-                    this.model.handleVote(3);
-                    break;
-                case 'k':
-                    e.preventDefault();
-                    this.model.handleVote(4);
-                    break;
-                case "Backspace":
-                case "Delete":
-                    e.preventDefault();
-                    this.model.undoLastVote();
-                    break;
-                case " ":
-                    e.preventDefault();
-                    this.model.toggleFlags();
-                    break;
-                default:
-                    break;
-            }
+        if (e.target !== document.body) {
+            return
+        }
+
+        switch (e.key) {
+            case 'd':
+                e.preventDefault();
+                this.model.handleVote(1);
+                break;
+            case 'f':
+                e.preventDefault();
+                this.model.handleVote(2);
+                break;
+            case 'j':
+                e.preventDefault();
+                this.model.handleVote(3);
+                break;
+            case 'k':
+                e.preventDefault();
+                this.model.handleVote(4);
+                break;
+            case "Backspace":
+            case "Delete":
+                e.preventDefault();
+                this.model.undoLastVote();
+                break;
+            case " ":
+                e.preventDefault();
+                this.model.toggleFlags();
+                break;
+            case "c":
+                this.model.handleFlag(1);
+            case "v":
+                this.model.handleFlag(2);
+            case "n":
+                this.model.handleFlag(3);
+            case "m":
+                this.model.handleFlag(4);
+            case "a":
+                this.model.handleAmbiguous();
+            case ";":
+                this.model.handleObvious();
+            case "Enter":
+                this.model.submit();
+            case "Escape":
+                this.model.resetState();
+            default:
+                break;
         }
     }
 }
